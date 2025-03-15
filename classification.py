370ebbbe0d5de8badd86137b87ccb9bc3ab7d311
MarchMol <marchmol98@gmail.com>
2025-03-14 22:42:14 -0600

    :feat: classification nb

--- classification.py
+++ classification.py
@@ -28,3 +28,18 @@ def breif_clustering(X, n_clusters):
     X['Cluster'] = km.fit_predict(X_pca)
     centroides = km.cluster_centers_
     return X
+
+def metrics_and_cm(y_pred, y_test):
+    # Presicion
+    accuracy = accuracy_score(y_test, y_pred)
+    print(f"Precisión del modelo: {accuracy:.2f}")
+    print(classification_report(y_test, y_pred))
+    
+    # Matriz de confusion
+    cm = confusion_matrix(y_test, y_pred)
+    plt.figure(figsize=(6, 5))
+    sns.heatmap(cm, annot=True, fmt="d", cmap="Blues", xticklabels=np.unique(y_test), yticklabels=np.unique(y_test))
+    plt.xlabel("Predicted")
+    plt.ylabel("Actual")
+    plt.title("Confusion Matrix")
+    plt.show()
--- regresion.py
+++ regresion.py
@@ -1,3 +1,6 @@
+import scipy.stats as stats
+import numpy as np
+
 def drop_many_nulls(df):
     df = df.copy()  # Evitar modificar el dataframe original
     
@@ -12,3 +15,16 @@ def drop_many_nulls(df):
     df = df.drop(columns=drop_columns, errors='ignore')
     
     return df
+
+def prueba_de_normalidad(column, column_name):
+    ks_statistic, p_value = stats.kstest(column, 'norm', args=(np.mean(column), np.std(column)))
+
+    # Imprimir los resultados
+    print(f"Estadístico de prueba (ks_statistic) = {ks_statistic:.20f}")
+    print(f"p-value = {p_value:.20f}")
+
+    alpha = 0.05
+    if p_value < alpha:
+        print(f"Se rechaza la hipótesis nula: los datos de '{column_name}' NO provienen de una distribución normal." + "\n")
+    else:
+        print(f"No se rechaza la hipótesis nula: los datos de '{column_name}' parecen provenir de una distribución normal." + "\n")
